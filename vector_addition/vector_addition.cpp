b130b6c61ec9483c25c56e7a0e30531acb4ee169
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2025-02-18 23:13:59 +0000

    :tada: vector addition example with expression templates and SIMD traits added.

--- vector_addition/vector_addition.cpp
+++ vector_addition/vector_addition.cpp
@@ -99,15 +99,95 @@ public:
         AlignedAlloc().deallocate(data_);
     }
 
+    Scalar &operator()(size_t i) { return data_[i]; }
+    const Scalar &operator()(size_t i) const { return data_[i]; }
+
+    size_t size() const { return size_; }
+    Scalar *data() { return data_; }
+    const Scalar *data() const { return data_; }
+
+    template <typename Expr>
+    Vector &operator=(const Expr &expr)
+    {
+        expr.eval(data_);
+        return *this;
+    }
+
 private:
     size_t size_;
     Scalar *data_;
 };
 
+// Expression template for vector addition
+template <typename Lhs, typename Rhs>
+class VectorSum
+{
+public:
+    VectorSum(const Lhs &lhs, const Rhs &rhs) : lhs_(lhs), rhs_(rhs) {}
+
+    size_t size() const
+    {
+        return lhs_.size();
+    }
+
+    // SIMD evaluation
+    void eval(double *result) const
+    {
+        using Traits = SimdTraits<double>;
+        using Packet = Traits::Packet;
+        constexpr size_t PacketSize = Traits::PacketSize;
+
+        size_t i = 0;
+        // Process elements in chunks of PacketSize
+        for (; i + PacketSize <= size(); i += PacketSize)
+        {
+            Packet a = Traits::load(&lhs_(i)); // Load packet
+            Packet b = Traits::load(&rhs_(i)); // Load packet
+            Packet c = Traits::add(a, b);      // Add packets
+            Traits::store(&result[i], c);      // Store packet
+        }
+
+        // Process remaining elements
+        for (; i < size(); ++i)
+        {
+            result[i] = lhs_(i) + rhs_(i);
+        }
+    }
+
+private:
+    const Lhs &lhs_;
+    const Rhs &rhs_;
+};
+
+// Overload the + Operator
+template <typename Lhs, typename Rhs>
+VectorSum<Lhs, Rhs> operator+(const Lhs &lhs, const Rhs &rhs)
+{
+    return VectorSum<Lhs, Rhs>(lhs, rhs);
+}
+
 int main()
 {
-    // const size_t size = 10; // Not a multiple of 4 (AVX PacketSize)
-    // Vector<double> v(size), w(size), u(size);
+    const size_t size = 10; // Not a multiple of 4 (AVX PacketSize)
+    Vector<double> v(size), w(size), u(size);
+
+    // Initialize vectors
+    for (size_t i = 0; i < size; ++i)
+    {
+        v(i) = static_cast<double>(i);
+        w(i) = static_cast<double>(i * 2);
+    }
+
+    // Perform vector addition
+    u = v + w;
+
+    // Print the result
+    std::cout << "u = v + w = ";
+    for (size_t i = 0; i < size; ++i)
+    {
+        std::cout << u(i) << " ";
+    }
+    std::cout << std::endl;
 
     using Traits = SimdTraits<double>;
     std::cout << "Packet size: " << Traits::PacketSize << std::endl;
