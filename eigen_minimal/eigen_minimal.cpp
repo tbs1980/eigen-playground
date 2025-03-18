373ebfb9b0a89d2012f3877e0aee15989250e77b
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2025-03-18 23:04:53 +0000

    :construction: added simple vector addition example.

---
+++ eigen_minimal/eigen_minimal.cpp
@@ -0,0 +1,9 @@
+#include <Eigen/Core>
+
+int main()
+{
+    int size = 50;
+    // VectorXf is a vector of floats, with dynamic size.
+    Eigen::VectorXf u(size), v(size), w(size);
+    u = v + w;
+}
